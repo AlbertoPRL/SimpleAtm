cb53ced8fd78b211bcc9feb50b9166642393ffa0
AlbertoPRL <apablorojaslopez@gmail.com>
2024-07-31 14:09:13 -0400

    feat: Added Basic Properties for an Account

--- src/Infrastructure/Identity/ApplicationUser.cs
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -3,4 +3,7 @@
 namespace SimpleAtm.Infrastructure.Identity;
 public class ApplicationUser : IdentityUser
 {
+    public long AccountNumber { get; set; }
+    public decimal Balance { get; set; }
+    public int Pin { get; set; }
 }

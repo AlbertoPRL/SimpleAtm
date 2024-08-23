a087600075c1bd1e402ca87d922ed1c1ba0fca8e
AlbertoPRL <apablorojaslopez@gmail.com>
2024-08-22 22:41:18 -0400

    add: MudBlazor

---
+++ src/BlazorClient/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor.Services;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBlazorServices(this IServiceCollection services)
+    {
+        services.AddMudServices();
+
+        return services;
+    }
+}
